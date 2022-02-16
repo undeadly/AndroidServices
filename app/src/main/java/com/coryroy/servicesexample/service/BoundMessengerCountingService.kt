b133e6b9c3eff390728ac0237715e4a17a346986
Cory Roy <cory.roy@masimo.com>
2022-02-16 11:57:41 -0800

    Cleanup BoundMessengerService

--- app/src/main/java/com/coryroy/servicesexample/service/BoundMessengerCountingService.kt
+++ app/src/main/java/com/coryroy/servicesexample/service/BoundMessengerCountingService.kt
@@ -10,59 +10,32 @@ import kotlinx.coroutines.*
 
 class BoundMessengerCountingService : Service(){
 
-    private lateinit var messenger : Messenger
-
-    override fun onBind(p0: Intent?): IBinder {
-        messenger = Messenger(IncomingHandler(this))
-        return messenger.binder
-    }
-
-    internal class IncomingHandler(context: Context, private val applicationContext : Context = context.applicationContext) : Handler() {
-
-        override fun handleMessage(msg: Message) {
-            when (msg.what) {
-                MSG_START -> startCounting()
-                MSG_STOP -> stopCounting()
-
-                else -> super.handleMessage(msg)
-
-            }
-        }
-
-        fun startCounting() {
-            countingJob = startCountingJob()
-        }
-
-        private fun startCountingJob() : Job {
-            return CoroutineScope(Dispatchers.Default).launch {
-                while (countingJob?.isActive != false) {
-                    delay(1000)
-                    val newCount = (CountingViewModel.count.value ?: 0) + 1
-
-                    Log.d("BndMsgCountService", "$newCount")
-                    CountingViewModel.count.postValue(newCount)
-                }
-            }
-        }
-
-        fun stopCounting() {
-            countingJob?.cancel()
-        }
-    }
-
-
-
-
-
-    override fun onDestroy() {
-        countingJob?.cancel()
-        super.onDestroy()
-    }
-
     companion object {
         const val MSG_START = 1
         const val MSG_STOP = 2
         var countingJob: Job? = null
 
+        private fun startCounting() { countingJob = startCountingJob() }
+        private fun stopCounting() { countingJob?.cancel() }
+        private fun startCountingJob() : Job {
+            return CoroutineScope(Dispatchers.Default).launch {
+                while (countingJob?.isActive != false) {
+                    delay(1000)
+                    val newCount = (CountingViewModel.count.value ?: 0) + 1
+                    CountingViewModel.count.postValue(newCount)
+                } } } }
+    
+    override fun onBind(intent: Intent?): IBinder = Messenger(IncomingHandler(this)).binder
+
+    internal class IncomingHandler(context: Context) : Handler(context.mainLooper) {
+        override fun handleMessage(msg: Message) {
+            when (msg.what) {
+                MSG_START -> startCounting()
+                MSG_STOP -> stopCounting()
+                else -> super.handleMessage(msg)
+            }
+        }
     }
+
+    override fun onDestroy() { countingJob?.cancel(); super.onDestroy() }
 }
